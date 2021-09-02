8fe9e9ddcd08a3fb2b160fd502a0b175ccdbfc45
Art3m198 <klass125@yandex.ru>
2021-09-02 20:23:13 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,75 @@
+
+var qrcodeAddress = new QRCode(document.getElementById("qrcodeAddress"),{width: 120,height: 120});
+var qrcodeSecret = new QRCode(document.getElementById("qrcodeSecret"),{width: 120, height: 120});
+
+newwatc();
+
+function getConfig() {
+	var networkConfigs = {
+		'BUTK': {
+			'uri': 'BUTK:',
+			'title': 'ButKoin Wallet',
+			'name': 'ButKoin',
+			'ticker': 'BUTK',
+			'network': {
+				'messagePrefix': '\x19BUTK Signed Message:\n',
+				'bip32': {
+					'public': 0x0488b21e,
+					'private': 0x0488ade4
+				},
+				'pubKeyHash': 0x4C,
+				'scriptHash': 0x10,
+				'wif': 0xCC
+			}
+		}
+	}
+	network=Object.keys(networkConfigs)[0]
+	return networkConfigs[network]
+}
+
+// Create new wallet
+function newwatc(){
+	var keys = bitcoin.ECPair.makeRandom({'network': getConfig()['network']})
+	var address = getAddress(keys)
+
+	if (address != undefined) {
+		var addrurl = "https://explorer.butcoin.tech/address/"+address;
+		document.getElementById("address").innerHTML = address;
+		document.getElementById("secret").innerHTML = keys.toWIF();
+		document.getElementById("address-top").innerHTML = address;
+		document.getElementById("secret-top").innerHTML = keys.toWIF();
+		document.getElementById("addr").href = addrurl;
+		qrcodeAddress.makeCode(address);
+		qrcodeSecret.makeCode(keys.toWIF());
+	}
+}
+
+function getAddress(keys) {
+	var network = getConfig()['network']
+	var address = undefined
+
+	
+	if (getAddressType() == 'bech32') {
+		address = bitcoin.payments.p2wpkh({
+			'pubkey': keys.publicKey,
+			'network': network
+		}).address
+	} else if (getAddressType() == 'segwit') {
+		address = bitcoin.payments.p2sh({
+			'redeem': getP2WPKHScript(keys.publicKey),
+			'network': network
+		}).address
+	} else if (getAddressType() == 'legacy') {
+		address = bitcoin.payments.p2pkh({
+			'pubkey': keys.publicKey,
+			'network': getConfig()['network']
+		}).address
+	}
+
+	return address
+}
+
+function getAddressType() {
+	var type = 'legacy'
+	return type
+}
